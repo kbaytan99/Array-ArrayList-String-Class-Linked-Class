d0a4b0c2b01317077dc22da0176aee9cb494ad3c
Alinur Kaan Baytan Erdoğan <57916206+Alinurkaanbaytanerdogan@users.noreply.github.com>
2020-02-12 19:29:03 +0100

    Update Arrayleri_Siralamak_Kopyalamak_Arrays_Sinifi_ve_Ek_Detaylar.java

--- Arrayleri_Siralamak_Kopyalamak_Arrays_Sinifi_ve_Ek_Detaylar.java
+++ Arrayleri_Siralamak_Kopyalamak_Arrays_Sinifi_ve_Ek_Detaylar.java
@@ -1,4 +1,5 @@
 import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Arrayleri_Siralamak_Kopyalamak_Arrays_Sinifi_ve_Ek_Detaylar {
@@ -18,12 +19,30 @@ public class Arrayleri_Siralamak_Kopyalamak_Arrays_Sinifi_ve_Ek_Detaylar {
 			System.out.println("ELEMENT: " + (i+1) + ": " + array[i]);
 		}
 	}
+	public static void array_sort(int [] array) {
+		//ARRAYS SINIFI
+		
+		Arrays.sort(array);
+		arrayi_bastir(array);
+		
+	}
 
 	public static void main(String[] args) {
 	
 		int[] a = arrayi_doldur(5);
-		arrayi_bastir(a);
+		//arrayi_bastir(a);
+		//array_sort(a); //SORT
 		
+		int[] a1 = {1,2,3,4,5,6};
+		int[] a2 = {1,2,3,4,5,6};
 		
+		if(Arrays.equals(a1, a2)) {
+			System.out.println("Esitler");
+		}
+		else {
+			System.out.println("Esit Degil");
+		}
+		//ESIT DEGILER CUNKU FARKLI YONLERI GOSTERIYORLAR
+		//EGER == SE ESIT DEGIL, EGER EQUALS SE ESIT
 	}
 }
